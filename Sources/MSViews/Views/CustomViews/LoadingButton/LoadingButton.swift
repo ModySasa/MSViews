23148b077c5fdbd36aa5f87eb80a4e08ae8e200d
Mohamed <mohamed.safwat@systemira.net>
2024-10-23 02:11:52 +0300

    Removed TextStyle from inverted

--- Sources/MSViews/Views/CustomViews/LoadingButton/LoadingButton.swift
+++ Sources/MSViews/Views/CustomViews/LoadingButton/LoadingButton.swift
@@ -96,7 +96,7 @@ public struct LoadingButton: View {
                                     Image(buttonImage)
                                 }
                                 Text(buttonText)
-                                    .textStyle(.button)
+//                                    .textStyle(.button)
                                     .foregroundColor(
                                         mainColor
                                     )

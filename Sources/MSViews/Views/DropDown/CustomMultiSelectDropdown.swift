61905192a1d39229289ac662eb3f098eea4def0c
Mohamed <mohamed.safwat@systemira.net>
2025-05-28 19:03:27 +0300

    fix 2

--- Sources/MSViews/Views/DropDown/CustomMultiSelectDropdown.swift
+++ Sources/MSViews/Views/DropDown/CustomMultiSelectDropdown.swift
@@ -11,26 +11,52 @@ public struct CustomMultiSelectDropdown<Label: View>: View {
     @Binding var selectedOptions: [String]
     var options: [String]
     let saveText: String
-    var checkBox : CheckBox
+    
+    let isChechBoxRound : Bool
+    let isCheckBoxFilled : Bool
+    
+    let checkTextColor : Color
+    let checkedColor : Color
+    let checkFillColor : Color
+    let unCheckedColor : Color
+     
+    let checkTextSize : Double
+    
+    let checkTextWeight : Font.Weight
+    
     
     var label: (Binding<[String]>) -> Label
     
     @State private var isPopoverPresented = false
 
     public init(
-        selectedOptions: Binding<[String]> ,
-        options: [String] ,
-        checkBox : CheckBox ,
-        saveText: String ,
-        label: @escaping (Binding<[String]>) -> Label
+        selectedOptions: Binding<[String]>,
+        options: [String],
+        saveText: String,
+        isChechBoxRound: Bool,
+        isCheckBoxFilled: Bool,
+        checkTextColor: Color,
+        checkedColor: Color,
+        checkFillColor: Color,
+        unCheckedColor: Color,
+        checkTextSize: Double,
+        checkTextWeight: Font.Weight,
+        label: @escaping (Binding<[String]>) -> Label,
     ) {
         self._selectedOptions = selectedOptions
         self.options = options
-        self.label = label
-        self.checkBox = checkBox
         self.saveText = saveText
+        self.isChechBoxRound = isChechBoxRound
+        self.isCheckBoxFilled = isCheckBoxFilled
+        self.checkTextColor = checkTextColor
+        self.checkedColor = checkedColor
+        self.checkFillColor = checkFillColor
+        self.unCheckedColor = unCheckedColor
+        self.checkTextSize = checkTextSize
+        self.checkTextWeight = checkTextWeight
+        self.label = label
     }
-    
+        
     public var body: some View {
         Button(action: {
             isPopoverPresented.toggle()
@@ -41,7 +67,30 @@ public struct CustomMultiSelectDropdown<Label: View>: View {
             ScrollView {
                 VStack(alignment: .leading, spacing: 10) {
                     ForEach(options, id: \.self) { option in
-                        checkBox
+                        CheckBox(
+                            isChecked: .init(get: {
+                                let isIn = selectedOptions.contains(option)
+                                print(isIn)
+                                return isIn
+                            }, set: { b in
+                                if(b ?? false) {
+                                    selectedOptions.append(option)
+                                } else {
+                                    selectedOptions.removeAll { s in
+                                        s == option
+                                    }
+                                }
+                            }),
+                            text: option,
+                            isRound: isChechBoxRound,
+                            isFilled: isCheckBoxFilled,
+                            textColor: checkTextColor,
+                            textSize: checkTextSize,
+                            textWeight: checkTextWeight,
+                            checkedColor: checkedColor,
+                            fillColor: checkFillColor,
+                            unCheckedColor: unCheckedColor
+                        )
                     }
                     Divider()
                     Text(

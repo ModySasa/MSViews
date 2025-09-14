3bef810066c221be8084d62ac6da77f1357a9405
Mo.Aziz <mohamed.aziz@systemira.net>
2025-09-14 17:17:37 +0300

    public

--- Sources/MSViews/Views/CustomToast/ToastMessageModel.swift
+++ Sources/MSViews/Views/CustomToast/ToastMessageModel.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-struct ToastMessage: Equatable {
+public struct ToastMessage: Equatable {
     public let id = UUID()
     public let type: CustomToastType
     public let title: String

7ce111e3ad3a214a8b54d02f3a82ba15a1d6cc1b
Mohamed <mohamed.safwat@systemira.net>
2025-11-10 10:28:10 +0200

    DynamicTextEditor

---
+++ Sources/MSViews/Views/DynamicTextEditor/DynamicTextEditorHeightView.swift
@@ -0,0 +1,55 @@
+//
+//  DynamicTextEditorHeightView.swift
+//  DeltaCapital
+//
+//  Created by Moha on 11/10/25.
+//
+
+
+import SwiftUI
+
+public struct DynamicTextEditorHeightView: View {
+    @Binding var text: String
+    @State private var textHeight: CGFloat = 38
+    @State private var fontSize: CGFloat = 38
+    @State private var fontWeight: Font.Weight = .medium
+
+    public init (
+        text: Binding<String>,
+        fontWeight: Font.Weight = .medium,
+        fontSize: CGFloat = 14
+    ) {
+        self._text = text
+        self.fontWeight = fontWeight
+        self.fontSize = fontSize
+    }
+    
+    public var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                TextEditor(text: $text)
+                    .onChange(of: text) { _ in
+                        if let font = UIFont(name: getAppFont(fontWeight), size: fontSize) {
+                            let width = geometry.size.width // match your .padding(4)
+                            textHeight = text.heightForTextEditor(width: width, font: font)
+                        }
+                    }
+            }
+        }
+        .frame(height: textHeight)
+    }
+}
+
+extension String {
+    func heightForTextEditor(width: CGFloat, font: UIFont) -> CGFloat {
+        let text = self.isEmpty ? " " : self
+        let constraintBox = CGSize(width: width - 8, height: .greatestFiniteMagnitude) // subtract padding
+        let boundingBox = text.boundingRect(
+            with: constraintBox,
+            options: [.usesLineFragmentOrigin, .usesFontLeading],
+            attributes: [.font: font],
+            context: nil
+        )
+        return ceil(boundingBox.height) + 16 // add top + bottom padding
+    }
+}
